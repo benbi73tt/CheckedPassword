62dc1cbe0a7c1390530ad17a0af70c5b35059ecb
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-11-10 15:45:29 +0400

    sshd and ip

--- src/main/java/com/example/checkedpassword/ResPassword.java
+++ src/main/java/com/example/checkedpassword/ResPassword.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import javafx.event.ActionEvent;
@@ -45,7 +47,8 @@ public class ResPassword {
 
         filChoose.setOnAction(new EventHandler<ActionEvent>() {
 
-            @FXML @Override
+            @FXML
+            @Override
             public void handle(ActionEvent event) {
                 selectedFile = fileChooser.showOpenDialog(new Stage());
                 nameFile.setText(selectedFile.getName());
@@ -65,12 +68,55 @@ public class ResPassword {
 
     }
 
-    private void read() throws Exception{
+    private void read() throws Exception {
         contents = readUsingFiles(String.valueOf(selectedFile));
         System.out.println(contents);
         System.out.println(contents.length());
+        String[] sshd;
+        String sshdString = null;
+        String[] ipAdress;
+        String adress = null;
+        String[] ports;
+
+
+        // sshd = contents.toString().split()
         String[] arr = contents.toString().split("\\s*(|-|_|—|,|\\.)\\s");
-        Collections.addAll(fileList, arr);
+        Pattern ip = Pattern.compile("(\\d{0,3}\\.){3}\\d{0,3}");
+        for (int i = 0; i < arr.length; i++) {
+            Matcher m = ip.matcher(arr[i]);
+            if (m.find()) {
+                System.out.println(m.group());
+                adress += m.group() + " ";
+            }
+        }
+        ipAdress = adress.split("\\s*(|-|_|—|,|\\.)\\s");
+
+        Pattern pattern = Pattern.compile("\\b*(sshd\\[\\d+\\])");
+        for (int i = 0; i < arr.length; i++) {
+            Matcher matcher = pattern.matcher(arr[i]);
+            System.out.println(matcher);
+            while (matcher.find()) {
+                String word = matcher.group();
+                if (word.toLowerCase().startsWith("s"))
+                    sshdString += word + " ";
+            }
+        }
+        sshd = sshdString.split("\\s*(|-|_|—|,|\\.)\\s");
+
+        Pattern portPattern = Pattern.compile("port");
+        for (int i = 0; i<arr.length;i++){
+            Matcher matcher = portPattern.matcher(arr[i]);
+            System.out.println(matcher);
+            while (matcher.find()) {
+                String word = matcher.group();
+                System.out.println(word);
+                if (word.toLowerCase().startsWith("p")){
+                    System.out.println(word);
+                }
+
+            }
+        }
+            Collections.addAll(fileList, sshd);
     }
 
     private static String readUsingFiles(String fileName) throws IOException {
@@ -78,12 +124,12 @@ public class ResPassword {
     }
 
     public List<String> duplicate() {
-        Map<String,Integer > counter = new HashMap<>();
-        for(String x : fileList){
-            int newValue = counter.getOrDefault(x,0) + 1;
-            counter.put(x,newValue);
+        Map<String, Integer> counter = new HashMap<>();
+        for (String x : fileList) {
+            int newValue = counter.getOrDefault(x, 0) + 1;
+            counter.put(x, newValue);
         }
-        return counter.entrySet().stream().sorted(Map.Entry.<String,Integer>comparingByValue()
+        return counter.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue()
                 .reversed()).map(it -> {
             return it.getKey() + " = " + it.getValue();
         }).collect(Collectors.toList());
